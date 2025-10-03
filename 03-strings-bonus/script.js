387f13a878c5c7a95d780f1293a778610ef05b2f
FedericoCanale <icocanale@gmail.com>
2025-10-03 14:59:52 +0200

    Applied some comments to bunus exercises

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -8,24 +8,27 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // il carattere Y nella variabile city
 const cityIndex = city.indexOf('Y');
 console.log(cityIndex); // Output: 4
+// Applied .indexOf to get the position of letter Y
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
 const citySubstring = city.substring(4);
 console.log(citySubstring); // Output: "York"
+// Applied .substring to print only York in the console
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
 const cityReplaced = city.replace("York", "Dehli");
 console.log(cityReplaced); // Output:  "New Delhi"
-
+// Applied .replace to replace York in the console with Dehli
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
 const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
-
+// Applied template literl to connect two variables in the console
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
 const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
+// Applied .toUpperCase to get Hello in uppercase in the console
